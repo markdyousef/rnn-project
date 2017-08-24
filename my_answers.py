5f8bd2f8c5d62b1655b9a4c44ae3b7751c58a0e8
markdyousef <markdyousef@gmail.com>
2017-08-24 08:45:55 +0200

    removed linear layer from model and retrained

--- my_answers.py
+++ my_answers.py
@@ -68,6 +68,5 @@ def window_transform_text(text, window_size, step_size):
 def build_part2_RNN(window_size, num_chars):
     model = Sequential()
     model.add(LSTM(200, input_shape=(window_size, num_chars)))
-    model.add(Dense(num_chars, activation='linear'))
     model.add(Dense(num_chars, activation='softmax'))
     return model
